814123ed106a32f83ec0504291b7092769d70b48
Peter Eigenschink <p.eigenschink@gmail.com>
2016-04-07 16:20:49 +0200

    Single test suite

--- test/spec/steganography.spec.js
+++ test/spec/steganography.spec.js
@@ -1,19 +1,17 @@
 describe('steganography.js', function(){
-  describe('Basics', function(){
-    it('Encode/decode message equality', function (done) {
-      var img = document.createElement('img');
-      img.onload = function() {
-        var msg = readJSON('json/message.json');
-        var dataURL = stego.encode(msg, img);
-        var readImg = document.createElement('img');
-        readImg.onload = function() {
-          var readMsg = stego.decode(readImg);
-          expect(readMsg).toEqual(msg);
-          done();
-        };
-        readImg.src=dataURL;
+  it('Encode/decode message equality', function (done) {
+    var img = document.createElement('img');
+    img.onload = function() {
+      var msg = readJSON('json/message.json');
+      var dataURL = stego.encode(msg, img);
+      var readImg = document.createElement('img');
+      readImg.onload = function() {
+        var readMsg = stego.decode(readImg);
+        expect(readMsg).toEqual(msg);
+        done();
       };
-      img.src = 'base/img/tshirts-20.jpg';
-    });
+      readImg.src=dataURL;
+    };
+    img.src = 'base/img/tshirts-20.jpg';
   });
 });
