a45b3b72d27dfa85ddfd02e3399809538b55a550
Peter Eigenschink <p.eigenschink@gmail.com>
2016-04-08 12:53:00 +0200

    Added test cases

--- test/spec/steganography.spec.js
+++ test/spec/steganography.spec.js
@@ -1,17 +1,51 @@
 describe('steganography.js', function(){
-  it('Encode/decode message equality', function (done) {
-    var img = document.createElement('img');
-    img.onload = function() {
-      var msg = readJSON('json/message.json');
-      var dataURL = stego.encode(msg, img);
-      var readImg = document.createElement('img');
-      readImg.onload = function() {
-        var readMsg = stego.decode(readImg);
+
+  describe('Encode/Decode consistency', function(){
+    function encodeDecode(cb, config) {
+      var img = document.createElement('img');
+      img.onload = function() {
+        var msg = readJSON('json/message.json');
+        var dataURL = steg.encode(msg, img, config);
+        var readImg = document.createElement('img');
+        readImg.onload = function() {
+          var readMsg = steg.decode(readImg, config);
+          cb(msg, readMsg);
+        };
+        readImg.src=dataURL;
+      };
+      img.src = 'base/img/tshirts-20.jpg';
+    }
+
+    it('is given using default config', function (done) {
+      encodeDecode(function(msg, readMsg) {
         expect(readMsg).toEqual(msg);
         done();
-      };
-      readImg.src=dataURL;
-    };
-    img.src = 'base/img/tshirts-20.jpg';
+      });
+    });
   });
+
+  describe('Encode', function(){
+    it('is defined', function () {
+      expect(steg.encode).not.toBeUndefined();
+    });
+  });
+
+  describe('Decode', function(){
+    it('is defined', function () {
+      expect(steg.decode).not.toBeUndefined();
+    });
+  });
+
+  describe('Capacity', function(){
+    it('is defined', function () {
+      expect(steg.getHidingCapacity).not.toBeUndefined();
+    });
+  });
+
+  describe('Config', function(){
+    it('is defined', function () {
+      expect(steg.config).not.toBeUndefined();
+    });
+  });
+
 });
